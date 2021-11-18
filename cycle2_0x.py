988890b9b478d9e8cd86ce5c7bcf6a4b5af89bad
Ramy Mohamed <ramy.mohamed@ibm.com>
2021-11-18 15:18:51 +0000

    Thu 18 Nov 2021 03:18:51 PM GMT

--- cycle2_0x.py
+++ cycle2_0x.py
@@ -178,7 +178,7 @@ class IoTMqtt(IoTSixfabTelit.IoT):
         # self.myMessage = "Hello 2025"
         # self.sendATComm("AT#MQPUBS=1,\"5G-Solutions\",0,0,"+self.myMessage+self.CTRL_Z,"OK")
         self.new_sensor_data = dict(itertools.islice(sensor_data.items(), 9))
-        print("len(self.new_sensor_data)", len(self.new_sensor_data))
+        print("len(json.dumps(self.new_sensor_data))", len(json.dumps(self.new_sensor_data)))
 
         # sensor_data = dict()
         # sensor_data['ID'] = 12
