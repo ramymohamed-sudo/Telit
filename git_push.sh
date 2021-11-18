ab53ceaf2816ad4652a660c73d74bb0cf1e0410a
Ramy Mohamed <ramy.mohamed@ibm.com>
2021-11-18 14:32:08 +0000

    Thu 18 Nov 2021 02:32:08 PM GMT

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-git add .
-git commit -m "$(date)"
-git push origin master
+hostname=$(hostname)
+# echo $hostname
+# echo "$PWD"
+
+if [ $hostname == 'k8s-virtual-machine' ]
+then
+echo "This is the analytical server"
+git add .;
+git commit -m "$(date)";
+git push origin master;
+
+else
+   echo "This is a Raspberry-PI "
+   git pull origin master
+
+fi
+
+
+# DIR="/home/k8s/minikube/github-scripts/Telit-and-BG96/BG96-final"
+# if [ -d "$DIR" ]; then
+#   echo "Installing config files in ${DIR}..."
+# fi
