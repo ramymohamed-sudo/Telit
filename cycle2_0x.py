0a7e20c5b9c977ae7a76cc824e4207913a0ac3b8
Ramy Mohamed <ramy.mohamed@ibm.com>
2021-11-18 16:46:52 +0000

    Thu 18 Nov 2021 04:46:52 PM GMT

--- cycle2_0x.py
+++ cycle2_0x.py
@@ -177,8 +177,9 @@ class IoTMqtt(IoTSixfabTelit.IoT):
         sleep(self.secs_befr_send)
         # self.myMessage = "Hello 2025"
         # self.sendATComm("AT#MQPUBS=1,\"5G-Solutions\",0,0,"+self.myMessage+self.CTRL_Z,"OK")
-        self.new_sensor_data = json.dumps(dict(itertools.islice(sensor_data.items(), 6)))
-        print("len(json.dumps(self.new_sensor_data))", len(json.dumps(self.new_sensor_data)))
+        self.new_sensor_data = json.dumps(dict(itertools.islice(sensor_data.items(), 6)))+self.CTRL_Z
+        print("self.new_sensor_data", self.new_sensor_data)
+        # print("len(json.dumps(self.new_sensor_data))", len(json.dumps(self.new_sensor_data)))
 
         # sensor_data = dict()
         # sensor_data['ID'] = 12
@@ -197,8 +198,8 @@ class IoTMqtt(IoTSixfabTelit.IoT):
         #                'hrs_sinc_chrg': 2,
         #                'chrg_cycl': '1'}
 
-        self.sendATComm(f"AT#MQPUBS=1,\"5G-Solutions\",0,0,\"{self.new_sensor_data}\""+self.CTRL_Z,"OK") # this also works well 
-        # self.sendATComm(f"AT#MQPUBS=1,\"5G-Solutions\",0,0,{self.new_sensor_data}"+self.CTRL_Z,"OK") 
+        # self.sendATComm(f"AT#MQPUBS=1,\"5G-Solutions\",0,0,\"{self.new_sensor_data}\""+self.CTRL_Z,"OK") # this also works well 
+        self.sendATComm("AT#MQPUBS=1,\"5G-Solutions\",0,0,{self.new_sensor_data}","OK") 
         # self.sendATComm("AT#MQPUBS=1,\"5G-Solutions\",0,0,"+self.new_sensor_data,"OK")
         
     def mqtt_close(self):
