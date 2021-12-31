221e1d2cef97c4af9342ca25ddaff57e9a71bcfe
Ramy Mohamed <ramy.mohamed@ibm.com>
2021-12-31 11:53:20 +0000

    Fri 31 Dec 2021 11:53:20 AM GMT

--- autoswitches/processor.py
+++ autoswitches/processor.py
@@ -38,8 +38,8 @@ class SensorData():
         self.sensor_data['name'] = name
         self.pijuice = PiJuice(1, 0x14)
         self.model= model
-        self.upper_threshold = 84.0
-        self.lower_threshold = 80.0
+        self.upper_threshold = 90.0
+        self.lower_threshold = 10.0
         
 
         self.sensor_id = [int(s) for s in name.split('-') if s.isdigit()][0]
